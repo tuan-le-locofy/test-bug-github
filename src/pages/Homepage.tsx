c40cec802c3c78c3634297fbf77898b9a898a100
tuan-le-locofy <tuan-le-locofy>
2024-01-31 09:02:44 +0000

    Pushed from Locofy Builder

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,265 @@
+import { FunctionComponent } from "react";
+import styles from "./Homepage.module.css";
+
+const Homepage: FunctionComponent = () => {
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.homepageChild} />
+      <div className={styles.right}>
+        <b className={styles.login}>Login</b>
+        <div className={styles.button}>
+          <b className={styles.login}>Sign Up</b>
+        </div>
+      </div>
+      <div className={styles.left}>
+        <img className={styles.logoIcon} alt="" src="/logo.svg" />
+        <div className={styles.menu}>
+          <b className={styles.login}>Pricing</b>
+          <b className={styles.login}>Resources Center</b>
+          <b className={styles.login}>About</b>
+          <b className={styles.login}>Contact</b>
+        </div>
+      </div>
+      <img className={styles.backgroundIcon} alt="" src="/background.svg" />
+      <img className={styles.patternIcon} alt="" src="/pattern@2x.png" />
+      <div className={styles.rightillustration}>
+        <div className={styles.illustration}>
+          <div className={styles.illustration1}>
+            <div className={styles.illustrations}>
+              <div className={styles.illustrationsChild} />
+              <div className={styles.illustrationsItem} />
+              <img className={styles.illustrationsInner} alt="" />
+              <div className={styles.instagramParent}>
+                <b className={styles.instagram}>Instagram</b>
+                <div className={styles.div}>12,62</div>
+              </div>
+              <img
+                className={styles.instagramIcon}
+                alt=""
+                src="/044instagram.svg"
+              />
+              <div className={styles.rectangleDiv} />
+              <div className={styles.illustrationsChild1} />
+              <div className={styles.illustrationsChild2} />
+            </div>
+            <div className={styles.illustrations1}>
+              <div className={styles.illustrationsChild3} />
+              <b className={styles.b}>800+</b>
+              <div className={styles.dailyVisitor}>Daily Visitor</div>
+            </div>
+            <div className={styles.illustrations2}>
+              <div className={styles.illustrationsChild3} />
+              <div className={styles.lineDiv} />
+              <div className={styles.illustrationsChild5} />
+              <div className={styles.illustrationsChild6} />
+              <div className={styles.illustrationsChild7} />
+              <div className={styles.illustrationsChild8} />
+              <div className={styles.kParent}>
+                <div className={styles.k}>60k</div>
+                <div className={styles.k1}>30k</div>
+                <div className={styles.k2}>10k</div>
+                <div className={styles.div1}>0</div>
+                <div className={styles.mar}>90k</div>
+              </div>
+              <div className={styles.marParent}>
+                <div className={styles.mar}>Mar</div>
+                <div className={styles.apr}>Apr</div>
+                <div className={styles.may}>May</div>
+                <div className={styles.jun}>Jun</div>
+                <div className={styles.jul}>Jul</div>
+              </div>
+              <b className={styles.dataAnalytics}>Data Analytics</b>
+              <img className={styles.groupIcon} alt="" src="/group-82.svg" />
+              <div className={styles.groupParent}>
+                <div className={styles.vectorParent}>
+                  <img
+                    className={styles.groupChild}
+                    alt=""
+                    src="/polygon-2.svg"
+                  />
+                  <div className={styles.groupItem} />
+                  <div className={styles.k4}>48k</div>
+                  <div className={styles.visitor}>Visitor</div>
+                  <img
+                    className={styles.groupInner}
+                    alt=""
+                    src="/group-87.svg"
+                  />
+                </div>
+                <div className={styles.ellipseDiv} />
+              </div>
+            </div>
+            <div className={styles.illustrations3}>
+              <div className={styles.illustrationsChild3} />
+              <img className={styles.slack1Icon} alt="" src="/079slack-1.svg" />
+              <div className={styles.totalSubscribeParent}>
+                <div className={styles.totalSubscribe}>Total Subscribe</div>
+                <b className={styles.b1}>61.000</b>
+              </div>
+            </div>
+            <div className={styles.illustrations4}>
+              <div className={styles.illustrationsChild3} />
+              <b className={styles.salesStats}>Sales Stats</b>
+              <div className={styles.illustrationsChild11} />
+              <div className={styles.illustrationsChild12} />
+              <div className={styles.illustrationsChild13} />
+              <div className={styles.illustrationsChild14} />
+              <img
+                className={styles.illustrationsChild15}
+                alt=""
+                src="/group-83.svg"
+              />
+            </div>
+            <div className={styles.illustrations5}>
+              <div className={styles.illustrationsChild3} />
+              <div className={styles.daily}>Daily</div>
+              <b className={styles.revenueGrowth}>Revenue Growth</b>
+              <img
+                className={styles.illustrationsChild17}
+                alt=""
+                src="/group-80.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.left1}>
+        <div className={styles.about1}>
+          <div className={styles.rating}>
+            <div className={styles.ratingOnGoogle}>
+              Rating on google play and app store
+            </div>
+            <b className={styles.b2}>4.8</b>
+          </div>
+          <div className={styles.company}>
+            <div className={styles.over500Business}>
+              Over 500 business powered with us
+            </div>
+            <b className={styles.b2}>350+</b>
+          </div>
+        </div>
+        <i className={styles.weWillNot}>
+          *we will not disseminate your email and so you avoid spam
+        </i>
+        <div className={styles.button1}>
+          <div className={styles.box}>
+            <div className={styles.yourBusinessEmail}>Your business email</div>
+          </div>
+          <div className={styles.button2}>
+            <b className={styles.login}>Get Started</b>
+          </div>
+        </div>
+        <div className={styles.theFastestAnd}>
+          The fastest and simple way to generate growing business solutions with
+          our products
+        </div>
+        <b className={styles.weHelpAny}>
+          We help any business to get analytics and sales marketing
+        </b>
+      </div>
+      <img className={styles.bgIcon} alt="" src="/bg.svg" />
+      <div className={styles.content}>
+        <div className={styles.copyright}>
+          <div className={styles.bg} />
+          <div className={styles.socialMedia}>
+            <img
+              className={styles.akarIconslinkedinFill}
+              alt=""
+              src="/akariconslinkedinfill.svg"
+            />
+            <img
+              className={styles.akarIconstwitterFill}
+              alt=""
+              src="/akariconstwitterfill.svg"
+            />
+            <img
+              className={styles.bxbxlInstagramAltIcon}
+              alt=""
+              src="/bxbxlinstagramalt.svg"
+            />
+          </div>
+          <div className={styles.growthlyAllRight}>
+            © 2020 Growthly. All Right Reserved
+          </div>
+        </div>
+        <div className={styles.shortlink}>
+          <div className={styles.resources}>
+            <div className={styles.tools}>Tools</div>
+            <div className={styles.support}>Support</div>
+            <div className={styles.blog}>Blog</div>
+            <div className={styles.guidesAndResources}>
+              Guides and resources
+            </div>
+            <b className={styles.resources1}>Resources</b>
+          </div>
+          <div className={styles.company1}>
+            <div className={styles.contact1}>Contact</div>
+            <div className={styles.partners}>Partners</div>
+            <div className={styles.termsConditions}>{`Terms & Conditions`}</div>
+            <div className={styles.privacyPolicy}>Privacy Policy</div>
+            <div className={styles.about2}>About</div>
+            <b className={styles.resources1}>Company</b>
+          </div>
+          <div className={styles.product}>
+            <div className={styles.features}>Features</div>
+            <div className={styles.benefits}>Benefits</div>
+            <div className={styles.pricing1}>Pricing</div>
+            <div className={styles.landingPages}>Landing pages</div>
+            <b className={styles.resources1}>Product</b>
+          </div>
+        </div>
+        <div className={styles.about3}>
+          <div className={styles.slack}>
+            <div className={styles.joinOurSlack}>Join our slack community</div>
+            <img className={styles.iconslack} alt="" src="/iconslack.svg" />
+          </div>
+          <div className={styles.email}>
+            <div className={styles.hellodhuhacreativecom}>
+              hello@dhuhacreative.com
+            </div>
+            <img className={styles.iconSystem} alt="" src="/icon-system.svg" />
+          </div>
+          <div className={styles.theFastestAnd1}>
+            The fastest and simple way to generate growing business solutions
+            with our products
+          </div>
+          <img className={styles.logoIcon1} alt="" src="/logo1.svg" />
+        </div>
+      </div>
+      <div className={styles.separator} />
+      <div className={styles.bg1} />
+      <div className={styles.content1}>
+        <div className={styles.pattern}>
+          <div className={styles.patternChild} />
+          <div className={styles.patternItem} />
+          <div className={styles.patternInner} />
+          <div className={styles.patternChild1} />
+          <div className={styles.patternChild2} />
+          <div className={styles.patternChild3} />
+          <div className={styles.patternChild4} />
+          <div className={styles.patternChild5} />
+          <div className={styles.patternChild6} />
+          <div className={styles.patternChild7} />
+          <div className={styles.patternChild8} />
+          <div className={styles.patternChild9} />
+        </div>
+        <div className={styles.buttonBar}>
+          <div className={styles.box}>
+            <div className={styles.yourBusinessEmail}>Your business email</div>
+          </div>
+          <div className={styles.button2}>
+            <b className={styles.login}>Get Started</b>
+          </div>
+        </div>
+        <div className={styles.theFastestAnd2}>
+          The fastest and simple way to generate growing business solutions with
+          our products
+        </div>
+        <b className={styles.readyToGet}>Ready to get started?</b>
+      </div>
+      <div className={styles.separto} />
+    </div>
+  );
+};
+
+export default Homepage;
